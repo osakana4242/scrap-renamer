f36d14fb43314c769708a6f7edbcb72431ed3243
osakana4242 <osakana4242@gmail.com>
2026-07-06 00:30:36 +0900

    feat: フルパス表示. 調子が悪い

--- ScrapRenamer/Editor/app.js
+++ ScrapRenamer/Editor/app.js
@@ -7,6 +7,9 @@ require.config({
 let scrapRenamer = {
 	editor: null,
 	pathDecorations: null,
+	lineCount: 0,
+	lines: [],
+	origPaths: [],
 };
 
 require([
@@ -28,7 +31,48 @@ require([
 			},
 		});
 
+	// Enterキーで改行ではなく次の行に移動する
+	scrapRenamer.editor.addCommand(
+		monaco.KeyCode.Enter,
+		() => {
+			scrapRenamer.editor.trigger("keyboard", "cursorDown", {});
+		}
+	);
 
+
+
+
+	scrapRenamer.lineCount = scrapRenamer.editor.getModel().getLineCount();
+
+	scrapRenamer.editor.onDidChangeModelContent((e) => {
+		console.log("Content changed:", e);
+		const model = scrapRenamer.editor.getModel();
+
+		if (model.getLineCount() !== scrapRenamer.lineCount) {
+			// 元に戻す
+			scrapRenamer.editor.trigger("keyboard", "undo", {});
+			console.log("Line count changed, undoing the change.");
+			return;
+		}
+
+		e.changes.forEach(change => {
+			console.log("Change:", change);
+			change.range.startLineNumber;
+		});
+
+		// ずれる
+		// scrapRenamer.editor.layout();
+		// const origPaths = scrapRenamer.origPaths;
+		// const ranges = scrapRenamer.editor.getVisibleRanges();
+		// const firstVisibleLine = ranges[0].startLineNumber;
+		// console.log(firstVisibleLine);
+		// document.querySelectorAll(".path-decoration").forEach((el, i) => {
+		// 	console.log("Setting data-path for decoration", i, origPaths[firstVisibleLine - 1 + i]);
+		// 	el.setAttribute("data-path", origPaths[firstVisibleLine - 1 + i]);
+		// });
+
+
+	});
 
 	window.addEventListener("dragover", e => {
 		e.preventDefault();
@@ -52,6 +96,111 @@ require([
 	});
 });
 
+// カーソルが decoration まで移動してしまう問題がある
+function refreshDecorations1() {
+	const decorations = [];
+	const model = scrapRenamer.editor.getModel();
+	const origPaths = scrapRenamer.origPaths;
+
+	for (let i = 0; i < origPaths.length; i++) {
+		console.log(
+			i + 1,
+			model.getLineContent(i + 1),
+			model.getLineMaxColumn(i + 1)
+		);
+
+		const lineMaxColumn = model.getLineMaxColumn(i + 1);
+		decorations.push({
+			range: new monaco.Range(
+				i + 1,
+				lineMaxColumn - 1,
+				i + 1,
+				lineMaxColumn
+			),
+			options: {
+				after: {
+					content: "|   " + origPaths[i],
+					inlineClassName: "path-decoration"
+				},
+				cursorStops: monaco.editor.InjectedTextCursorStops.BEFORE
+			}
+		});
+	}
+
+	if (scrapRenamer.pathDecorations) {
+		scrapRenamer.pathDecorations.clear();
+	}
+
+	scrapRenamer.pathDecorations =
+		scrapRenamer.editor.createDecorationsCollection(decorations);
+	scrapRenamer.editor.layout();
+}
+
+// 見た目、操作感ともこれが最高なのだが、
+// 行を編集したとたんに、装飾が消えてしまう。
+function refreshDecorations2() {
+	const decorations = [];
+	const model = scrapRenamer.editor.getModel();
+	const origPaths = scrapRenamer.origPaths;
+
+	for (let i = 0; i < origPaths.length; i++) {
+		console.log(
+			i + 1,
+			model.getLineContent(i + 1),
+			model.getLineMaxColumn(i + 1)
+		);
+
+		const lineMaxColumn = model.getLineMaxColumn(i + 1);
+		decorations.push({
+			range: new monaco.Range(
+				i + 1,
+				lineMaxColumn - 1,
+				i + 1,
+				lineMaxColumn
+			),
+			options: {
+				afterContentClassName: "path-decoration",
+				// after: {
+				// 	content: "|    " + origPaths[i], // "C:\\Users\\me\\Documents\\foo.txt",
+				// 	inlineClassName: "path-decoration",
+				// 	cursorStops: monaco.editor.InjectedTextCursorStops.NEVER,
+				// 	zIndex: 3,
+				// }
+			}
+		});
+	}
+
+	if (scrapRenamer.pathDecorations) {
+		scrapRenamer.pathDecorations.clear();
+	}
+
+	// scrapRenamer.pathDecorations =
+	// 	scrapRenamer.editor.createDecorationsCollection(decorations);
+
+	scrapRenamer.pathDecorations =
+		scrapRenamer.editor.createDecorationsCollection(decorations);
+	scrapRenamer.editor.layout();
+
+	// document.querySelectorAll(".path-decoration").forEach((el, i) => {
+	// 	console.log("Setting data-path for decoration", i, origPaths[i]);
+	// 	el.setAttribute("data-path", origPaths[i]);
+	// });
+
+	const ranges = scrapRenamer.editor.getVisibleRanges();
+
+	const firstVisibleLine = ranges[0].startLineNumber;
+	console.log(firstVisibleLine);
+	document.querySelectorAll(".path-decoration").forEach((el, i) => {
+		console.log("Setting data-path for decoration", i, origPaths[firstVisibleLine - 1 + i]);
+		el.setAttribute("data-path", origPaths[firstVisibleLine - 1 + i]);
+	});
+
+}
+
+function refreshDecorations() {
+	refreshDecorations1();
+}
+
 window.chrome.webview.addEventListener("message", e => {
 	console.log(e.data);
 
@@ -71,34 +220,11 @@ window.chrome.webview.addEventListener("message", e => {
 		case "setLines": {
 			const text = e.data.lines.
 				join("\n");
-			scrapRenamer.editor.setValue(
-				text);
-
-			const decorations = [];
-
-			for (let i = 0; i < e.data.origPaths.length; i++) {
-
-				decorations.push({
-					range: new monaco.Range(i + 1, 1, i + 1, 1),
-					options: {
-						isWholeLine: true,
-						after: {
-							content: "HOGE", //e.data.origPaths[i], // "C:\\Users\\me\\Documents\\foo.txt",
-							inlineClassName: "path-decoration"
-						}
-					}
-				});
-			}
-
-			if (scrapRenamer.pathDecorations) {
-				scrapRenamer.pathDecorations.clear();
-			}
-
-			scrapRenamer.pathDecorations =
-				scrapRenamer.editor.createDecorationsCollection(decorations);
-
-
-
+			scrapRenamer.lineCount = e.data.lines.length;
+			scrapRenamer.editor.setValue(text);
+			scrapRenamer.origPaths = e.data.origPaths;
+			scrapRenamer.lines = e.data.lines;
+			refreshDecorations();
 			break;
 		}
 	}
