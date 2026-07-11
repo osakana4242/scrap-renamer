4556756a4b355cf4113a51591896eaeed3bb22d5
osakana4242 <osakana4242@gmail.com>
2026-07-11 13:03:39 +0900

    refactor: 名前交換

--- ScrapRenamer/MainWindow/Line.cs
+++ ScrapRenamer/MainWindow/Line.cs
@@ -1,7 +1,18 @@
+using System.Diagnostics;
+
 namespace ScrapRenamer;
 
 class Line {
 	public string origPath = "";
 	public string editedLine = "";
-	public string error = "";
+	string _error = "";
+	public string error {
+		get => _error;
+		set {
+			if (value != "") {
+				Debug.WriteLine($"error: {value}");
+			}
+			_error = value;
+		}
+	}
 }
--- ScrapRenamer/MainWindow/LineContainer.cs
+++ ScrapRenamer/MainWindow/LineContainer.cs
@@ -31,197 +31,213 @@ class LineContainer {
 		});
 	}
 
-	record class WorkItem(int index, string before, string after);
-
 	public void Apply() {
+		var context = new RenameContext(this);
+		context.Apply();
+	}
 
-		List<WorkItem> workList = new();
-		Dictionary<string, WorkItem> beforePathDict = new();
-		Dictionary<string, WorkItem> afterPathDict = new();
+	// リネームのパターン
+	//
+	// Chain
+	// 末尾から解決してけば衝突しない
+	//
+	// 1.
+	// a -> b
+	//
+	// 2.
+	// a -> b
+	// b -> c
+	// 
+	// 3.
+	// a -> b
+	// b -> c
+	// c -> d
+	//
+	// Cycle
+	// どこかで一時的な名前を経由しないと解決できない
+	//
+	// 1. (リネームしてない)
+	// a -> a
+	//
+	// 2.
+	// a -> b
+	// b -> a
+	//
+	// 3.
+	// a -> b
+	// b -> c
+	// c -> a
+	//
+	class RenameContext {
+		readonly List<WorkItem> _workList = new();
+		readonly Dictionary<string, WorkItem> _beforePathDict = new();
+		readonly Dictionary<string, WorkItem> _afterPathDict = new();
+		readonly LineContainer _owner;
 
-		for (int i = 0; i < Lines.Count; i++) {
-			var line = Lines[i];
-			if (line.origPath == line.editedLine) {
-				continue;
-			}
-			Debug.WriteLine($"Renaming: {line.origPath} -> {line.editedLine}");
-			var nextPath = line.editedLine;
-			switch (_mode) {
+
+		IReadOnlyList<Line> Lines => _owner.Lines;
+
+
+		public RenameContext(LineContainer owner) {
+			_owner = owner;
+		}
+
+		public void Apply() {
+			Debug.WriteLine("Apply");
+			BuildWorkList();
+			// Chain を片付ける
+			ProcessChain();
+			// 残りは Cycle
+			ProcessCycle();
+		}
+
+		void RemoveWorkItem(WorkItem item) {
+			_workList.Remove(item);
+			_beforePathDict.Remove(item.before);
+			_afterPathDict.Remove(item.after);
+		}
+
+		Line GetLine(WorkItem item) => Lines[item.index];
+
+		string GetNextPath(Line line) {
+			switch (_owner._mode) {
 			case Mode.Name:
-				nextPath = Path.Combine(Path.GetDirectoryName(line.origPath) ?? "", line.editedLine);
-				break;
+				return Path.Combine(Path.GetDirectoryName(line.origPath) ?? "", line.editedLine);
 			case Mode.FullPath:
-				nextPath = line.editedLine;
-				break;
+				return line.editedLine;
+			default:
+				throw new System.NotSupportedException($"{_owner._mode}");
 			}
-			WorkItem item = new(i, line.origPath, nextPath);
-
-			if (afterPathDict.TryGetValue(nextPath, out var otherItem)) {
-				line.error = $"{otherItem.index}: {otherItem.before} とリネーム先が衝突";
-				var otherLine = Lines[otherItem.index];
-				otherLine.error = $"{item.index}: {item.before} とリネーム先が衝突";
-				continue;
-			}
-			afterPathDict.Add(nextPath, item);
-			beforePathDict.Add(line.origPath, item);
-			workList.Add(item);
 		}
 
-
-		List<WorkItem> chainList = new();
-
-		// リネームのパターン
-		//
-		// Chain
-		// 末尾から解決してけば衝突しない
-		//
-		// 1.
-		// a -> b
-		//
-		// 2.
-		// a -> b
-		// b -> c
-		// 
-		// 3.
-		// a -> b
-		// b -> c
-		// c -> d
-		//
-		// Cycle
-		// どこかで一時的な名前を経由しないと解決できない
-		//
-		// 1. (リネームしてない)
-		// a -> a
-		//
-		// 2.
-		// a -> b
-		// b -> a
-		//
-		// 3.
-		// a -> b
-		// b -> c
-		// c -> a
-		//
-
-
-		// Chain を片付ける
-		WorkItem? chainFirst;
-		while ((chainFirst = FindChainFirst(workList, afterPathDict)) != null) {
-			chainList.Clear();
-			var item1 = chainFirst;
-			chainList.Add(item1);
-			while (beforePathDict.TryGetValue(item1.after, out var item2)) {
-				chainList.Add(item2);
-				item1 = item2;
+		void BuildWorkList() {
+			// エラーをリセット
+			for (int i = 0; i < Lines.Count; i++) {
+				var line = Lines[i];
+				line.error = "";
 			}
-			string preError = "";
-			for (int j = chainList.Count - 1; 0 <= j; j--) {
-				var item = chainList[j];
-				var line = Lines[item.index];
-				try {
-					if (line.error != "") {
-						preError = line.error;
-					} else if (preError != "") {
-						line.error = preError;
-					} else {
-						System.IO.File.Move(item.before, item.after);
-						line.origPath = item.after;
-					}
-				} catch (System.Exception ex) {
-					line.error = $"{ex.Message}";
-					preError = $"{line.origPath} のリネーム失敗に引きずられて失敗";
-				} finally {
-					workList.Remove(item);
-					beforePathDict.Remove(item.before);
-					afterPathDict.Remove(item.after);
+			//
+			for (int i = 0; i < Lines.Count; i++) {
+				var line = Lines[i];
+				if (line.origPath == line.editedLine) {
+					continue;
 				}
+				var nextPath = GetNextPath(line);
+				Debug.WriteLine($"Renaming: {line.origPath} -> {line.editedLine} ({nextPath})");
+				WorkItem item = new(i, line.origPath, nextPath);
+				if (item.before == item.after) {
+					// リネームしてない
+					continue;
+				}
+
+				if (_afterPathDict.TryGetValue(nextPath, out var otherItem)) {
+					line.error = $"{otherItem.index}: {otherItem.before} とリネーム先が衝突";
+					var otherLine = GetLine(otherItem);
+					otherLine.error = $"{item.index}: {item.before} とリネーム先が衝突";
+					continue;
+				}
+				_afterPathDict.Add(nextPath, item);
+				_beforePathDict.Add(line.origPath, item);
+				_workList.Add(item);
 			}
 		}
 
-		// 残りは Cycle
-		HashSet<string> visited = new();
-		while (0 < workList.Count) {
-			chainList.Clear();
-			visited.Clear();
-			{
-				var item1 = workList[0];
+		void ProcessChain() {
+			List<WorkItem> chainList = new();
+			WorkItem? chainFirst;
+			while ((chainFirst = FindChainFirst()) != null) {
+				chainList.Clear();
+				var item1 = chainFirst;
 				chainList.Add(item1);
-				visited.Add(item1.before);
-				while (beforePathDict.TryGetValue(item1.after, out var item2)) {
-					var isCycle = visited.Contains(item2.after);
+				while (_beforePathDict.TryGetValue(item1.after, out var item2)) {
 					chainList.Add(item2);
-					visited.Add(item2.before);
-					if (isCycle) {
-						break;
-					}
 					item1 = item2;
 				}
-			}
-			var preError = "";
-			var chainLast = chainList[chainList.Count - 1];
-			{
-				var tmpPath = Path.Combine(
-							Path.GetDirectoryName(chainLast.after)!,
-							Guid.NewGuid().ToString("N"));
-				var line = Lines[chainLast.index];
-				try {
-					if (line.error != "") {
-						preError = line.error;
-					} else if (preError != "") {
-						line.error = preError;
-					} else {
-						System.IO.File.Move(chainLast.before, tmpPath);
-						line.origPath = tmpPath;
-
-						var chainLast2 = new WorkItem(chainLast.index, tmpPath, chainLast.after);
-						chainList.Insert(0, chainLast2);
-					}
-				} catch (System.Exception ex) {
-					line.error = $"{ex.Message}";
-					preError = $"{line.origPath} のリネーム失敗に引きずられて失敗";
-				} finally {
-					workList.Remove(chainLast);
-					beforePathDict.Remove(chainLast.before);
-					afterPathDict.Remove(chainLast.after);
-				}
-			}
-
-			for (int j = chainList.Count - 1; 0 <= j; j--) {
-				var item = chainList[j];
-				var line = Lines[item.index];
-				try {
-					if (line.error != "") {
-						preError = line.error;
-					} else if (preError != "") {
-						line.error = preError;
-					} else {
-						System.IO.File.Move(item.before, item.after);
-						line.origPath = item.after;
-					}
-				} catch (System.Exception ex) {
-					line.error = $"{ex.Message}";
-					preError = $"{line.origPath} のリネーム失敗に引きずられて失敗";
-				} finally {
-					workList.Remove(item);
-					beforePathDict.Remove(item.before);
-					afterPathDict.Remove(item.after);
+				string preError = "";
+				for (int j = chainList.Count - 1; 0 <= j; j--) {
+					var item = chainList[j];
+					RenameItem(item, ref preError);
 				}
 			}
 		}
-	}
 
-	static WorkItem? FindChainFirst(
-		IReadOnlyList<WorkItem> workList,
-		IReadOnlyDictionary<string, WorkItem> afterPathSet) {
+		void ProcessCycle() {
+			List<WorkItem> cycleList = new();
+			HashSet<string> visited = new();
+			while (0 < _workList.Count) {
+				cycleList.Clear();
+				visited.Clear();
+				{
+					var item1 = _workList[0];
+					cycleList.Add(item1);
+					visited.Add(item1.before);
+					while (_beforePathDict.TryGetValue(item1.after, out var item2)) {
+						var isCycle = visited.Contains(item2.after);
+						cycleList.Add(item2);
+						visited.Add(item2.before);
+						if (isCycle) {
+							break;
+						}
+						item1 = item2;
+					}
+				}
+				
+				var cycleLast = cycleList[cycleList.Count - 1];
 
-		for (int i = 0; i < workList.Count; i++) {
-			var item1 = workList[i];
-			var isChainFirst = !afterPathSet.ContainsKey(item1.before);
-			if (isChainFirst) {
-				return item1;
+				// 工程を2つに分割する
+				// 1. 最初に一時的なパスに変更
+				// before -> tmp
+				var suffix = ".tmp_" + Guid.NewGuid().ToString("N");
+				var tmpPath = $"{cycleLast.after}{suffix}";
+				var lastBeforeToTmp = new WorkItem(cycleLast.index, cycleLast.before, tmpPath);
+				// 2. 最後に目的のパスに変更
+				// tmp -> after
+				var lastTmpToAfter = new WorkItem(cycleLast.index, tmpPath, cycleLast.after);
+				RemoveWorkItem(cycleLast);
+
+				var preError = "";
+
+				RenameItem(lastBeforeToTmp, ref preError);
+				for (int j = cycleList.Count - 1; 0 <= j; j--) {
+					var item = cycleList[j];
+					RenameItem(item, ref preError);
+				}
+				RenameItem(lastTmpToAfter, ref preError);
 			}
 		}
-		return null;
-	}
 
+		void RenameItem(WorkItem item, ref string preError) {
+			var line = GetLine(item);
+			try {
+				if (line.error != "") {
+					preError = line.error;
+				} else if (preError != "") {
+					line.error = preError;
+				} else {
+					System.IO.File.Move(item.before, item.after);
+					line.origPath = item.after;
+				}
+			} catch (System.Exception ex) {
+				line.error = $"{ex.Message}";
+				preError = $"{line.origPath} のリネーム失敗に引きずられて失敗";
+				Debug.WriteLine($"ex: {ex}, before: {item.before}, after: {item.after}");
+			} finally {
+				RemoveWorkItem(item);
+			}
+		}
+
+		WorkItem? FindChainFirst() {
+
+			for (int i = 0; i < _workList.Count; i++) {
+				var item1 = _workList[i];
+				var isChainFirst = !_afterPathDict.ContainsKey(item1.before);
+				if (isChainFirst) {
+					return item1;
+				}
+			}
+			return null;
+		}
+
+		record class WorkItem(int index, string before, string after);
+	}
 }
