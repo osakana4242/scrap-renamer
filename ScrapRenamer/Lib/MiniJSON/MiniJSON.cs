cd07e6a6a6a01f49aea61a3b4a84e1b7ef8c9966
osakana4242 <osakana4242@gmail.com>
2026-08-31 01:01:54 +0900

    docs:

--- ScrapRenamer/Lib/MiniJSON/MiniJSON.cs
+++ ScrapRenamer/Lib/MiniJSON/MiniJSON.cs
@@ -25,6 +25,9 @@
  * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * ----
+ * https://gist.github.com/darktable/1411710 を元に更新。
+ * pretty 引数で整形できるようにする。
  */
 #nullable disable
 
