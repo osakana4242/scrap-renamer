fa406d8d45fad8be765b807057d81c7466397230
osakana4242 <osakana4242@gmail.com>
2026-09-22 10:23:44 +0900

    feat: publish 時の html, js にコメントを含めないようにする

---
+++ ScrapRenamer/Views/MainWindow/Editor/build.js
@@ -0,0 +1,52 @@
+﻿const fs = require("fs");
+const path = require("path");
+const esbuild = require("esbuild");
+
+const sourceDirectory = path.join(__dirname, "dev");
+const outputDirectory = path.join(__dirname, "min");
+
+async function main() {
+	fs.rmSync(outputDirectory, {
+		recursive: true,
+		force: true,
+	});
+
+	fs.cpSync(sourceDirectory, outputDirectory, {
+		recursive: true,
+	});	
+	
+	// js のコメント除去、 minify 化をする
+	await esbuild.build({
+		entryPoints: [
+			path.join(sourceDirectory, "app.js"),
+		],
+		outfile: path.join(outputDirectory, "app.js"),
+		bundle: false,
+		minify: true,
+	});
+
+	// html と css のコメントを除去する
+	const html = fs.readFileSync(
+		path.join(sourceDirectory, "index.html"),
+		"utf8",
+	);
+
+	const htmlWithoutComments = html.replace(
+		/<!--[\s\S]*?-->/g,
+		"",
+	).replace(
+		/\/\*[\s\S]*?\*\//g,
+		"",
+	);
+
+	fs.writeFileSync(
+		path.join(outputDirectory, "index.html"),
+		htmlWithoutComments,
+	);
+}
+
+main().catch(error => {
+	console.error(error);
+	process.exit(1);
+});
+
